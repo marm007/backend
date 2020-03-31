bf292fb8d5c2a4d25d5f1bdd49aeb9ccf3f424eb
marm007 <dhkm.gornica@poczta.fm>
2020-03-31 04:29:26 +0200

    initial message

--- backend/settings.py
+++ backend/settings.py
@@ -30,6 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
