f6b59664c1539956ad2a5b5c5a02f5243c47404d
marm007 <dhkm.gornica@poczta.fm>
2020-04-02 19:46:48 +0200

    initial message

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -146,6 +146,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'backend.pagination.CustomPagination',
 }
 
 SIMPLE_JWT = {
