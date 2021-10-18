e1d28da5e6609170fe4a798008507d01d99d8303
marm007 <dhkm.gornica@poczta.fm>
2021-10-18 11:53:35 +0200

    chore: production settings changed

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -217,11 +217,16 @@ USE_TZ = True
 
 COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # WHITENOISE_USE_FINDERS = True
