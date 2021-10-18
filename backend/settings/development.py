3270914eb6a5fda23f530117d9127c747511c12b
marm007 <dhkm.gornica@poczta.fm>
2021-10-18 11:50:27 +0200

    chore: settings changed

--- backend/settings/development.py
+++ backend/settings/development.py
@@ -205,7 +205,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
--- backend/settings/production.py
+++ backend/settings/production.py
@@ -221,7 +221,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # WHITENOISE_USE_FINDERS = True
